99aefd901601f3fe1e8d89a89da2e294c021e9c9
Mustafa Emre Uçaklı <73758831+mustafaemreucakli@users.noreply.github.com>
2022-05-28 16:26:00 +0300

    Add files via upload

---
+++ Perfect Number Calculator/Main.java
@@ -0,0 +1,28 @@
+package Projects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number, sum = 0;
+
+        System.out.print("Please enter the number: ");
+        number = input.nextInt();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is a perfect number.");
+        }
+        else {
+            System.out.println(number + " is not a perfect number.");
+        }
+
+    }
+}
